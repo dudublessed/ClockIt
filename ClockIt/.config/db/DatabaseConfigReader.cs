0b2456b2267ad238986f40081bd97cfa8a4ea820
dudublessed <idicasapple@icloud.com>
2025-02-20 12:25:19 -0300

    feat: db connection config

---
+++ ClockIt/.config/db/DatabaseConfigReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+public class DatabaseConfigReader
+{
+    private static string _connectionString;
+
+    public static string GetConnectionString()
+    {
+        if (!string.IsNullOrEmpty(_connectionString)) {
+            return _connectionString;
+        }
+
+        try
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(@".config/db/DatabaseConfig.xml");
+
+            XmlNode node = xmlDoc.SelectSingleNode("//DatabaseConfig/ConnectionString");
+
+            if (node == null) throw new Exception(ExceptionHandler.GetErrorMessages(4161));
+
+            _connectionString = node.InnerText;
+            return _connectionString;
+
+        } catch (Exception ex)
+        {
+            throw new Exception(ExceptionHandler.GetErrorMessages(5161));
+        }
+    }   
+
+
+}

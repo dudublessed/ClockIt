78fd7970585dd19dfd6841fc074466f5cb9f75bd
dudublessed <idicasapple@icloud.com>
2025-02-22 00:05:09 -0300

    chore: include log file

---
+++ ClockIt/.config/log/exceptions/ExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+
+public class ExceptionHandler
+{
+    private static string _pathtofile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExceptionsPattern.xml");
+
+    public static string GetErrorMessages(int exceptionCode)
+    {
+        try
+        {
+            XDocument xmlDoc = XDocument.Load(_pathtofile);
+
+            var exception = xmlDoc.Descendants("Exception")
+                                         .FirstOrDefault(e => (string)e.Attribute("id") == exceptionCode.ToString());
+
+            if (exception == null) return "Non-existent Error OR Error not found.";
+
+            string exceptionMessage = exception.Element("Message").Value;
+            string exceptionPC = exception.Element("PC").Value;
+
+            return exceptionMessage + exceptionPC;
+        }
+        catch (Exception ex)
+        {
+            return $"Fatal error: {ex.Message}";
+        }
+    }
+}
